df7416b2b4a4c13e14fbd083b85e44685734fe88
niralittle <ua.katerynka@gmail.com>
2014-06-10 18:59:49 +0300

    Added WorkflowException

--- src/java/axiom/workflow/WorkflowException.java
+++ src/java/axiom/workflow/WorkflowException.java
@@ -1,9 +1,28 @@
 package axiom.workflow;
 
-/**
- *
- * @author Nira
- */
 public class WorkflowException extends RuntimeException {
 
+    /**
+     * Creates a new instance of <code>WorkflowException</code> without detail message.
+     */
+    public WorkflowException() {
+    }
+
+    /**
+     * Constructs an instance of <code>WorkflowException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public WorkflowException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * Constructs an instance of <code>WorkflowException</code> with the specified detail message
+     * and inner exception
+     * @param msg the detail message.
+     * @param innerException inner exception
+     */
+    public WorkflowException(String msg, Exception innerException) {
+        super(msg, innerException);
+    }
 }

ac22f411eb8c948d2c455e35e13d9f261bad1b3d
ariyana <arriyana@gmail.com>
2014-06-17 22:15:41 +0300

    Search

--- src/java/axiom/controllers/Search.java
+++ src/java/axiom/controllers/Search.java
@@ -4,6 +4,21 @@ package axiom.controllers;
  *
  * @author Nira
  */
-public class Search extends Controller {
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+public class Search extends HttpServlet{
+
+    @Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=ANCI");
+
+    }
 
 }

d89590229114f6612ac3899d961fba4b4b5079a3
ariyana <arriyana@gmail.com>
2014-06-19 03:43:15 +0300

    gui

--- tmp/jquery.js
+++ tmp/jquery.js
@@ -1,5 +1,6 @@
+
 $(document).ready(function() {
-    $('.tabs .tab-links a').on('click', function(e)  {
+    $('.tabs .tab-links a').on('click', function(e) {
         var currentAttrValue = $(this).attr('href');
 
         // Show/Hide Tabs
@@ -11,3 +12,63 @@ $(document).ready(function() {
         e.preventDefault();
     });
 });
+
+function OnOff(obj)
+		{
+			var
+				DivOn,
+				DivOff,
+				Ctrl;
+		
+			switch(obj.id)
+			{
+				case "user" :
+				{
+					DivOn="forUser";
+					DivOff="forStartup";
+					break;
+				}
+				case "startup" :
+				{
+					DivOn="forStartup";
+					DivOff="forUser";
+					break;
+				}
+			}
+	
+			if(Ctrl=document.getElementById(DivOn))
+				Ctrl.style.display="block";
+			if(Ctrl=document.getElementById(DivOff))
+				Ctrl.style.display="none";
+			DivOff="Result";
+			if(Ctrl=document.getElementById(DivOff))
+				Ctrl.style.display="none";	
+			
+		}
+function Find(obj)
+		{
+			var
+				DivOn,
+				Ctrl;
+		
+			DivOn="Result";
+			
+			if(Ctrl=document.getElementById(DivOn))
+				Ctrl.style.display="block";
+
+		}		
+function g(img,flag)
+		{
+			var
+				Ctrl;
+			if(Ctrl=document.getElementById("logo"))	
+				if (flag=0){
+					document.img.border="8px solid #CAD958";
+					flag=1;
+				}
+				else{
+					document.img.border="4px solid #CAD958";
+					flag=0;
+				}
+			
+		}
